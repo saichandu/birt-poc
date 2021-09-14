4fe2b0ff70f610f2f2ff07f376dd3751cb511f60
saichandu <avvarusaichandu@gmail.com>
2021-09-15 00:24:44 +0530

    Initial project upload

---
+++ src/test/java/com/birt/engine/SpringContextTest.java
@@ -0,0 +1,13 @@
+package com.birt.engine;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringContextTest {
+    
+    @Test
+    public void whenSpringContextIsBootstrapped_thenNoExceptions() {
+
+    }
+}
